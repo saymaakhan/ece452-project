56496b807b182dc61afc5a90fc813c7fdfe21916
Ryan Kugler <kuglerryan@gmail.com>
2023-06-27 20:19:13 -0400

    trying to integrate calendar application after login

---
+++ Ace/app/build/generated/data_binding_trigger/debug/com/example/ace/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.ace;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ Ace/app/src/main/java/com/example/ace/calendar/extensions/DateExtensions.kt
@@ -0,0 +1,9 @@
+package com.applandeo.calendarsampleapp.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Date.toSimpleDate(): String {
+    val simpleDateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+    return simpleDateFormat.format(this)
+}
