238372d4d6a2031525fcb7253470f272ea519c42
PrishaRathi <PrishaRathi@users.noreply.github.com>
2023-07-20 10:32:57 -0400

    Created class ChatContactAdapter and implemented chat contact recyclerview

---
+++ Ace/app/src/main/java/com/example/ace/ui/chat/ChatContactAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.ace.ui.chat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ace.R
+
+import com.example.ace.data.model.ChatUser
+
+class ChatContactAdapter(private val mList: MutableList<ChatUser>, val clickListener : (ChatUser, Int) -> Unit) : RecyclerView.Adapter<ChatContactAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_chat_contact, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = mList[position]
+        holder.bind(user)
+
+        holder.itemView.setOnClickListener { clickListener(user,position) }
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    open class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        private val profilePicImage : ImageView =  itemView.findViewById(R.id.profile_picture)
+        private val contactNameText : TextView = itemView.findViewById(R.id.contact_name)
+        open fun bind (user : ChatUser) {
+            contactNameText.setText(user.userName)
+            profilePicImage.setImageResource(R.drawable.sharp_person_24)
+        }
+    }
+}
