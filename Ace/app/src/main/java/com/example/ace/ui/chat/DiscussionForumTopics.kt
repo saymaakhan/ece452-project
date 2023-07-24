45527d0bae9007635ef9affce83991f7b09cc715
Emilykissoon <emilykissoon@hotmail.com>
2023-07-24 01:06:27 -0400

    make discussion page show user specific courses

--- Ace/app/src/main/java/com/example/ace/ui/chat/DiscussionForumTopics.kt
+++ Ace/app/src/main/java/com/example/ace/ui/chat/DiscussionForumTopics.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.compose.ui.text.toLowerCase
 import com.example.ace.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,32 +21,16 @@ import com.google.firebase.ktx.Firebase
 class DiscussionForumTopics : AppCompatActivity() {
     private lateinit var binding: ActivityDiscussionForumTopicsBinding
     private lateinit var auth: FirebaseAuth
-
-    var topics : MutableList<DiscussionForumTopic> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityDiscussionForumTopicsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
         loadCoursesFromFirestore()
-
-        val recyclerview : RecyclerView = findViewById<RecyclerView>(R.id.recycler_discussion_forum_topics)
-        recyclerview.layoutManager= LinearLayoutManager(this)
-        val adapter = DiscussionForumTopicsAdapter(topics){ topic: DiscussionForumTopic, position: Int ->
-            val topicName = topic.topicName
-            val intent = Intent(this, DiscussionForumChat::class.java)
-            intent.putExtra("topic", topicName )
-            startActivity(intent)
-            finish()
-        }
-        recyclerview.adapter = adapter
     }
     private fun getUserName(): String? {
         val user = auth.currentUser
-        return if (user != null) {
-            user.displayName
-        } else SingleChat.ANONYMOUS
+        return user?.uid ?: SingleChat.ANONYMOUS
     }
     private fun loadCoursesFromFirestore() {
         val firebaseAuth = FirebaseAuth.getInstance()
@@ -53,31 +38,44 @@ class DiscussionForumTopics : AppCompatActivity() {
         val recyclerview : RecyclerView = findViewById<RecyclerView>(R.id.recycler_discussion_forum_topics)
         recyclerview.layoutManager= LinearLayoutManager(this)
         val currUser = getUserName()
+        Log.d(TAG, "curr user: $currUser")
         if (userId != null) {
             val firestore = FirebaseFirestore.getInstance()
-            firestore.collection("user_enrolled_classes")
+            val classes = firestore.collection("user_enrolled_classes")
+            classes
                 .get()
                 .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    var topics : MutableList<DiscussionForumTopic> = mutableListOf()
                     Log.d(TAG,"SUCCESSFUL: ${task.result.size()}")
                     for (document in task.result) {
-                        topics.add(DiscussionForumTopic(document.id))
-                        Log.d(TAG, "Discussion: $document")
-                        Log.d(TAG,"SUCCESSFUL discussion")
+                        val docID = document.id
+                        val users = classes.document(docID).collection("users")
+                        users.get().addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                for (user in it.result) {
+                                    val userID = user.data["uid"].toString()
+                                    if (userID == currUser) {
+                                        Log.d(TAG, "Discussion name: $docID")
+                                        topics.add(DiscussionForumTopic(docID))
+                                    }
+                                }
+                            }
+                            val adapter =
+                                DiscussionForumTopicsAdapter(topics) { topic: DiscussionForumTopic, position: Int ->
+                                    val topicName = topic.topicName
+                                    val intent = Intent(this, DiscussionForumChat::class.java)
+                                    intent.putExtra("topic", topicName)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                            recyclerview.adapter = adapter
+                        }
                     }
-                    val adapter = DiscussionForumTopicsAdapter(topics){ topic: DiscussionForumTopic, position: Int ->
-                        val topicName = topic.topicName
-                        val intent = Intent(this, DiscussionForumChat::class.java)
-                        intent.putExtra("topic", topicName )
-                        startActivity(intent)
-                        finish()
-                    }
-                    recyclerview.adapter = adapter
-                    Log.d(ContentValues.TAG, topics.toString())
                 }
 
                 else {
-                    Log.d(ContentValues.TAG, "Error getting documents: ", task.exception)
+                    Log.d(TAG, "Error getting documents: ", task.exception)
                 }
             }
         }
